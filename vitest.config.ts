bd9d9632b779cd76b4d5f69d8cf98c2862dd6a6f
João Camargo <jclaudio00_12@hotmail.com>
2025-01-04 09:03:12 -0300

    adjusts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,5 @@
-import swc from 'unplugin-swc';
-import { defineConfig } from 'vitest/config';
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
@@ -13,4 +13,4 @@ export default defineConfig({
       module: { type: 'es6' },
     }),
   ],
-});
+})
